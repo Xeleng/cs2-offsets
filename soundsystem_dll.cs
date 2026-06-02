895ecc6c006889197205ec9d892617a69d2395d3
Xeleng <bmmygod11@gmail.com>
2026-06-02 12:57:18 +0700

    Auto update offsets Tue 06/02/2026 12:57:18.49

--- soundsystem_dll.cs
+++ soundsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-29 19:57:19.101377 UTC
+// 2026-06-02 05:57:17.824143800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: soundsystem.dll
